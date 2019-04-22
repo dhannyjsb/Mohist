f34143bb902ed9f7f308a3df1e267f1fba44dd3d
Mgazul <mgazul@163.com>
2019-04-22 22:20:09 +0800

    Player and Chat PAI

---
+++ src/main/java/cn/pfcraft/api/PlayerAPI.java
@@ -0,0 +1,18 @@
+package cn.pfcraft.api;
+
+import org.bukkit.craftbukkit.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+
+public class PlayerAPI {
+
+    /**
+     *  Get Player ping
+     *
+     * @param player org.bukkit.entity.player
+     */
+    public static String getPing(Player player){
+        CraftPlayer cp = ((CraftPlayer)player);
+        int ping = cp.getHandle().ping;
+        return String.valueOf(ping);
+    }
+}
