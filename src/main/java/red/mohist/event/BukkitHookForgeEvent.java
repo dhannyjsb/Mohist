ecfb29d934c61fe711f1f648ca0979456b8488ce
Mgazul <mgazul@163.com>
2019-05-08 05:50:08 +0800

    Add BukkitHookForgeEvent

---
+++ src/main/java/red/mohist/event/BukkitHookForgeEvent.java
@@ -0,0 +1,36 @@
+package red.mohist.event;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.HandlerList;
+
+import net.minecraftforge.fml.common.eventhandler.Event;
+
+public class BukkitHookForgeEvent extends org.bukkit.event.Event {
+    private static final HandlerList handlers = new HandlerList();
+    private final Event event;
+
+    public BukkitHookForgeEvent(Event event) {
+        super(!Bukkit.getServer().isPrimaryThread());
+        this.event = event;
+    }
+
+    /**
+     * @return Forge and Mods Event
+     */
+    public Event getEvent() {
+        return this.event;
+    }
+
+    public String getEventName() {
+        return event.getClass().getSimpleName();
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+}
