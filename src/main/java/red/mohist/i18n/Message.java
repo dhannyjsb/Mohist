cc294f1749f422f572507be614ef33f0b1d6fcbd
lliioollcn <44719954+lliioollcn@users.noreply.github.com>
2019-05-25 12:38:47 +0800

    Update Message.java

--- src/main/java/red/mohist/i18n/Message.java
+++ src/main/java/red/mohist/i18n/Message.java
@@ -126,7 +126,7 @@ public enum Message {
 
     private final String value;
     //public static ResourceBundle rb = ResourceBundle.getBundle("assets.mohist.lang.message", new UTF8Control());
-    public static ResourceBundle rb;
+    public static ResourceBundle rb = Mohist.rb;
 
 
     public static String getString(Message key){
