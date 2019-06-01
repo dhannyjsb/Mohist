f5f6d376e2c6523176ed3f847e6631b768044ca0
lliioollcn <lliiooll@126.com>
2019-06-01 22:34:45 +0800

    change download url

--- src/main/java/red/mohist/down/DownloadServer.java
+++ src/main/java/red/mohist/down/DownloadServer.java
@@ -15,7 +15,8 @@ public class DownloadServer implements Runnable {
 
     @Override
     public void run() {
-        String url = "https://s3.amazonaws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar";
+        String url = "https://launcher.mojang.com/v1/objects/886945bfb2b978778c3a0288fd7fab09d315b25f/server.jar";
+        //String url = "https://s3.amazonaws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar";
         Locale locale = Locale.getDefault();
         if (locale.getCountry().equals("zh_CN") || locale.getCountry().equals("CN") || locale.getCountry().equals("zh_TW") || locale.getCountry().equals("TW")) {
             url = "https://bmclapi2.bangbang93.com/version/1.12.2/server";
