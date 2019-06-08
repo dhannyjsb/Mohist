93facd084b34b36c5c562d9ef1abbb5869f9ce93
梦幻星空 <47418954+CraftDream@users.noreply.github.com>
2019-06-08 11:21:45 +0800

    add WatchMohist

---
+++ src/main/java/red/mohist/WatchMohist.java
@@ -0,0 +1,45 @@
+package red.mohist;
+
+import java.util.*;
+import net.minecraft.server.MinecraftServer;
+import red.mohist.i18n.Message;
+
+public class WatchMohist extends TimerTask
+{
+    private static Timer timer;
+    private static long Time;
+    private static long WarnTime;
+    
+    @Override
+    public void run() {
+        long curTime = System.currentTimeMillis();
+        if (WatchMohist.Time > 0L && curTime - WatchMohist.Time > 2000L && curTime - WatchMohist.WarnTime > 30000L) {
+            WatchMohist.WarnTime = curTime;
+            Mohist.LOGGER.warn(Message.getString(Message.WatchMohist_1));
+            Mohist.LOGGER.warn(Message.getFormatString(Message.WatchMohist_2, new Object[]{(curTime - WatchMohist.Time)}));
+            Mohist.LOGGER.warn(Message.getString(Message.WatchMohist_3));
+            for (StackTraceElement stack : MinecraftServer.getServerInst().primaryThread.getStackTrace()) {
+                Mohist.LOGGER.warn(Message.getString(Message.WatchMohist_4) + stack);
+            }
+            Mohist.LOGGER.warn(Message.getString(Message.WatchMohist_1));
+        }
+    }
+    
+    public static void update() {
+        WatchMohist.Time = System.currentTimeMillis();
+    }
+    
+    public static void start() {
+        WatchMohist.timer.schedule(new WatchMohist(), 30000L, 500L);
+    }
+    
+    public static void stop() {
+        WatchMohist.timer.cancel();
+    }
+    
+    static {
+        WatchMohist.timer = new Timer();
+        WatchMohist.Time = 0L;
+        WatchMohist.WarnTime = 0L;
+    }
+}
