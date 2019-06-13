3ab5ba19124d6a83bea69a4688bbab30425540e5
Mgazul <mgazul@163.com>
2019-06-13 21:30:28 +0800

    Fix error

--- src/main/java/org/bukkit/craftbukkit/v1_12_R1/entity/CraftProjectile.java
+++ src/main/java/org/bukkit/craftbukkit/v1_12_R1/entity/CraftProjectile.java
@@ -1,6 +1,6 @@
 package org.bukkit.craftbukkit.v1_12_R1.entity;
 
-import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.projectile.EntityThrowable;
 import org.bukkit.craftbukkit.v1_12_R1.CraftServer;
 import org.bukkit.entity.EntityType;
@@ -18,7 +18,7 @@ public class CraftProjectile extends AbstractProjectile implements Projectile {
 
     public void setShooter(ProjectileSource shooter) {
         if (shooter instanceof CraftLivingEntity) {
-            getHandle().thrower = (EntityLiving) ((CraftLivingEntity) shooter).entity;
+            getHandle().thrower = (EntityLivingBase) ((CraftLivingEntity) shooter).entity;
             if (shooter instanceof CraftHumanEntity) {
                 getHandle().throwerName = ((CraftHumanEntity) shooter).getName();
             }
