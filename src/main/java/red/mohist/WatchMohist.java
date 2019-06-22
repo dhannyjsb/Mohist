6967f514f2cd16efae197c979dddd240e6266400
Mgazul <mgazul@163.com>
2019-06-22 21:30:45 +0800

    Fix main thread stack trace does not work
    修复主线程堆栈跟踪不起作用(手动创建ScheduledThreadPoolExecutor线程池)

--- src/main/java/red/mohist/WatchMohist.java
+++ src/main/java/red/mohist/WatchMohist.java
@@ -1,16 +1,18 @@
 package red.mohist;
 
 import net.minecraft.server.MinecraftServer;
+import org.apache.commons.lang3.concurrent.BasicThreadFactory;
 import red.mohist.i18n.Message;
 
 import java.util.TimerTask;
-import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 public class WatchMohist extends TimerTask
 {
-    private static ScheduledExecutorService timer = Executors.newScheduledThreadPool(2);
+    private static ScheduledExecutorService timer = new ScheduledThreadPoolExecutor(1,
+            new BasicThreadFactory.Builder().namingPattern("appgw-schedule-update-cache-pool-%d").daemon(true).build());
     private static long Time = 0L;
     private static long WarnTime = 0L;
     
@@ -20,7 +22,7 @@ public class WatchMohist extends TimerTask
         if (WatchMohist.Time > 0L && curTime - WatchMohist.Time > 2000L && curTime - WatchMohist.WarnTime > 30000L) {
             WatchMohist.WarnTime = curTime;
             Mohist.LOGGER.warn(Message.getString(Message.WatchMohist_1));
-            Mohist.LOGGER.warn(Message.getFormatString(Message.WatchMohist_2, new Object[]{(curTime - WatchMohist.Time)}));
+            Mohist.LOGGER.warn(Message.getFormatString(Message.WatchMohist_2, new Object[]{ String.valueOf(curTime - WatchMohist.Time) }));
             Mohist.LOGGER.warn(Message.getString(Message.WatchMohist_3));
             for (StackTraceElement stack : MinecraftServer.getServerInst().primaryThread.getStackTrace()) {
                 Mohist.LOGGER.warn(Message.getString(Message.WatchMohist_4) + stack);
@@ -34,10 +36,10 @@ public class WatchMohist extends TimerTask
     }
     
     public static void start() {
-        timer.scheduleWithFixedDelay(new WatchMohist(), 30000L, 500L, TimeUnit.SECONDS);
+        timer.scheduleAtFixedRate(new WatchMohist(), 30000L, 500L, TimeUnit.MILLISECONDS);
     }
     
     public static void stop() {
-        timer.shutdownNow();
+        timer.shutdown();
     }
 }
