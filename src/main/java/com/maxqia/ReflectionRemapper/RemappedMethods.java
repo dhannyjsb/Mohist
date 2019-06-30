ae350da07678dd792e226e94f20c135dd3ba2530
Mgazul <mgazul@163.com>
2019-06-30 23:40:51 +0800

    Filter remap detection packet path

--- src/main/java/com/maxqia/ReflectionRemapper/RemappedMethods.java
+++ src/main/java/com/maxqia/ReflectionRemapper/RemappedMethods.java
@@ -27,26 +27,39 @@ public class RemappedMethods {
 
     // Get Fields
     public static Field getField(Class<?> inst, String name) throws NoSuchFieldException, SecurityException {
-        return inst.getField(Transformer.remapper.mapFieldName(
-                reverseMap(inst), name, null));
+        if (!inst.getName().startsWith("net.minecraft.")) {
+            return inst.getField(name);
+        }
+        return inst.getField(Transformer.remapper.mapFieldName(reverseMap(inst), name, null));
     }
 
     public static Field getDeclaredField(Class<?> inst, String name) throws NoSuchFieldException, SecurityException {
-        return inst.getDeclaredField(Transformer.remapper.mapFieldName(
-                reverseMap(inst), name, null));
+        if (!inst.getName().startsWith("net.minecraft.")) {
+            return inst.getDeclaredField(name);
+        }
+        return inst.getDeclaredField(Transformer.remapper.mapFieldName(reverseMap(inst), name, null));
     }
 
     // Get Methods
     public static Method getMethod(Class<?> inst, String name, Class<?>... parameterTypes) throws NoSuchMethodException, SecurityException {
+        if (!inst.getName().startsWith("net.minecraft.")) {
+            return inst.getMethod(name, parameterTypes);
+        }
         return inst.getMethod(mapMethod(inst, name, parameterTypes), parameterTypes);
     }
 
     public static Method getDeclaredMethod(Class<?> inst, String name, Class<?>... parameterTypes) throws NoSuchMethodException, SecurityException {
+        if (!inst.getName().startsWith("net.minecraft.")) {
+            return inst.getDeclaredMethod(name, parameterTypes);
+        }
         return inst.getDeclaredMethod(mapMethod(inst, name, parameterTypes), parameterTypes);
     }
 
     // getName
     public static String getName(Field field) {
+        if (!field.getDeclaringClass().getName().startsWith("net.minecraft.")) {
+            return field.getName();
+        }
         String name = field.getName();
         String match = reverseMap(field.getDeclaringClass());
 
@@ -64,6 +77,9 @@ public class RemappedMethods {
     }
 
     public static String getName(Method method) {
+        if (!method.getDeclaringClass().getName().startsWith("net.minecraft.")) {
+            return method.getName();
+        }
         String name = method.getName();
         String match = reverseMap(method.getDeclaringClass());
 
@@ -82,6 +98,9 @@ public class RemappedMethods {
 
     // Package names
     public static String getName(Package inst) {
+        if (!inst.getName().startsWith("net.minecraft.")) {
+            return inst.getName();
+        }
         if (inst == null)
         {
             return null;
@@ -103,11 +122,10 @@ public class RemappedMethods {
 
     // getSimpleName
     public static String getSimpleName(Class<?> inst) {
-        String[] name = getName(inst).split("\\.");
+        if (!inst.getName().startsWith("net.minecraft.")) {
+            return inst.getSimpleName();
+        }
+        String[] name = reverseMapExternal(inst).split("\\.");
         return name[name.length - 1];
-    }
-
-    public static String getName(Class<?> inst) {
-        return reverseMapExternal(inst);
     }
 }
