44f5fe4d8ec5b07cf771261cd51e9c0e68d5e7e6
Mgazul <mgazul@163.com>
2019-07-13 00:01:07 +0800

    Try fixing the onBlockClicked(you'll need to test)

--- src/main/java/red/mohist/api/PlayerAPI.java
+++ src/main/java/red/mohist/api/PlayerAPI.java
@@ -1,6 +1,8 @@
 package red.mohist.api;
 
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.server.MinecraftServer;
 import org.bukkit.craftbukkit.v1_12_R1.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
@@ -32,5 +34,10 @@ public class PlayerAPI {
 
     public static String getModlist(Player player) {
         return modlist.get(getNMSPlayer(player)) == null ? "null" : modlist.get(getNMSPlayer(player));
+    }
+
+    public static boolean isOp(EntityPlayer ep)
+    {
+        return MinecraftServer.getServerInst().getPlayerList().canSendCommands(ep.getGameProfile());
     }
 }
