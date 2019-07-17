6900e06ee76fb16117c7f32791d065b06448d193
Mgazul <mgazul@163.com>
2019-07-17 23:23:27 +0800

    修复sendPacket兼容性

--- src/main/java/red/mohist/common/asm/ASMClassTransformer.java
+++ src/main/java/red/mohist/common/asm/ASMClassTransformer.java
@@ -3,7 +3,14 @@ package red.mohist.common.asm;
 import net.minecraft.launchwrapper.IClassTransformer;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.tree.ClassNode;
+
+import static org.objectweb.asm.Opcodes.ACC_PUBLIC;
+import static org.objectweb.asm.Opcodes.ACC_SUPER;
+import static org.objectweb.asm.Opcodes.ALOAD;
+import static org.objectweb.asm.Opcodes.INVOKESPECIAL;
+import static org.objectweb.asm.Opcodes.RETURN;
 
 public class ASMClassTransformer implements IClassTransformer
 {
@@ -25,8 +32,18 @@ public class ASMClassTransformer implements IClassTransformer
     private byte[] transformClass(byte[] basicClass) {
         ClassNode classNode = new ClassNode();
         new ClassReader(basicClass).accept(classNode, 0);
-        ClassWriter classWriter = new ClassWriter(1);
-        classNode.access = 33;
+        ClassWriter classWriter = new ClassWriter(ClassWriter.COMPUTE_MAXS);
+
+        MethodVisitor mv = classWriter.visitMethod(ACC_PUBLIC, "sendPacket", "(Lnet/minecraft/network/Packet;)V", "(Lnet/minecraft/network/Packet<*>;)V", null);
+        mv.visitCode();
+        mv.visitVarInsn(ALOAD, 0);
+        mv.visitVarInsn(ALOAD, 1);
+        mv.visitMethodInsn(INVOKESPECIAL, "net/minecraft/network/NetHandlerPlayServer", "func_147359_a", "(Lnet/minecraft/network/Packet;)V", true);
+        mv.visitInsn(RETURN);
+        mv.visitEnd();
+
+        classNode.access = ACC_SUPER + ACC_PUBLIC;
+
         classNode.accept(classWriter);
         return classWriter.toByteArray();
     }
