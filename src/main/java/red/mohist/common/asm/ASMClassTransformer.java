66acd751d06b8d17c2be25a424399de78a2f4795
Mgazul <mgazul@163.com>
2019-07-29 22:49:06 +0800

    Update ASMClassTransformer.java
    处理mod在服务器使用客户端的类

--- src/main/java/red/mohist/common/asm/ASMClassTransformer.java
+++ src/main/java/red/mohist/common/asm/ASMClassTransformer.java
@@ -6,9 +6,18 @@ import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.MethodNode;
+
+import java.util.function.Predicate;
 
 public class ASMClassTransformer implements IClassTransformer
 {
+    private final Predicate<? super MethodNode> CLIENTSIDE = (method) ->
+            method.desc.contains("Lnet/minecraft/client/") && (
+                    method.desc.contains("Lnet/minecraft/client/util/ITooltipFlag") ||
+                            (method.desc.contains("Lnet/minecraft/client/renderer/") && !method.desc.contains("Lnet/minecraft/client/renderer/block/model/ModelResourceLocation"))
+            );
+
     public ASMClassTransformer() {
         super();
     }
@@ -17,6 +26,15 @@ public class ASMClassTransformer implements IClassTransformer
     public byte[] transform(String name, String transformedName, byte[] basicClass) {
         if (basicClass == null) {
             return basicClass;
+        }
+        ClassNode classNode = new ClassNode();
+        ClassReader classReader = new ClassReader(basicClass);
+        classReader.accept(classNode, 0);
+
+        if (classNode.methods.removeIf(CLIENTSIDE)) {
+            ClassWriter writer = new ClassWriter(0);
+            classNode.accept(writer);
+            return writer.toByteArray();
         }
         if ("net.minecraftforge.fml.common.network.handshake.NetworkDispatcher$1".equals(transformedName)) {
             basicClass = this.transformClass(basicClass);
