ef4c091022da8936b34ec103ac5d43a7ebd1d7e1
Mgazul <mgazul@163.com>
2019-08-13 18:10:48 +0800

    1.3 (1.2 has been around for a long time and decided to upgrade it)

--- src/main/java/red/mohist/Mohist.java
+++ src/main/java/red/mohist/Mohist.java
@@ -14,7 +14,7 @@ import java.util.concurrent.Executors;
 public class Mohist {
 
     private static final String NAME = "Mohist";
-    private static final String VERSION = "1.2";
+    private static final String VERSION = "1.3";
     private static final String NATIVE_VERSON = "v1_12_R1";
     private static final String NMS_PREFIX = "net/minecraft/server/";
     public static Logger LOGGER;
